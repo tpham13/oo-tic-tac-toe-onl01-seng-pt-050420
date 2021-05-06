acdec70115de2b5cee84f9cf61a3f1a078434452
tpham13 <tpham0123@gmail.com>
2021-05-05 22:39:41 -0700

    added display_board method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,8 +8,18 @@ class TicTacToe
   [2,5,8],
   [0,4,8],
   [2,4,6]
+
   def initialize
     @board = Array.new([" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
 
 end
